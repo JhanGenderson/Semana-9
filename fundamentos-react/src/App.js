101191c9f09bed3ab8add9d4b13d952544a738f0
JhanGenderson <jhan96.rc@gmail.com>
2021-05-21 18:56:50 -0500

    update

--- fundamentos-react/src/App.js
+++ fundamentos-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Codigo!!!!!
+          Hola React!!
         </p>
         <a
           className="App-link"
